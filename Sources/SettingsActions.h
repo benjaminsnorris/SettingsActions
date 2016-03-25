76978a7b6ea5f9060a2fbc3fd0e73eb8f5c1eb1b
Ben Norris <ben@bsn.io>
2016-03-25 10:58:46 -0700

    Add blank framework

---
+++ Sources/SettingsActions.h
@@ -0,0 +1,19 @@
+//
+//  SettingsActions.h
+//  SettingsActions
+//
+//  Created by Ben Norris on 3/25/16.
+//  Copyright © 2016 BSN Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SettingsActions.
+FOUNDATION_EXPORT double SettingsActionsVersionNumber;
+
+//! Project version string for SettingsActions.
+FOUNDATION_EXPORT const unsigned char SettingsActionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SettingsActions/PublicHeader.h>
+
+
