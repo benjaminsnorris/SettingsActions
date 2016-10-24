2f467074e6d55d1cd5408fe76d2337fad19bc43c
Ben Norris <code@bsn.design>
2016-10-24 08:47:13 -0600

    Convert to Swift 3 syntax

--- Sources/SettingsActionsService.swift
+++ Sources/SettingsActionsService.swift
@@ -20,7 +20,7 @@ public struct SettingsActionService {
     
     // MARK: - Constants
     
-    private let ratingLinkPathPrefix = "itms-apps://itunes.apple.com/app/id"
+    fileprivate let ratingLinkPathPrefix = "itms-apps://itunes.apple.com/app/id"
     
     
     // MARK: - Initializers
@@ -38,32 +38,32 @@ public struct SettingsActionService {
         let supportInfo = "iOS \(deviceInfoService.osVersion) on \(deviceInfoService.deviceName) \nLocale: \(deviceInfoService.locale) (\(deviceInfoService.language)) \n\(versionNumberService.appNameWithVersion))"
         let messageText = "Here are my thoughts:\n\n\n\n\n\n--------------------------------\nDeveloper Support Information\n\n\(supportInfo)\n--------------------------------\n"
         feedback.setMessageBody(messageText, isHTML: false)
-        viewController.presentViewController(feedback, animated: true, completion: nil)
+        viewController.present(feedback, animated: true, completion: nil)
     }
     
-    public func shareApp(fromViewController viewController: UIViewController, message: String? = nil, appStoreAppPath: String, completion: ((activityType: String?) -> Void)? = nil) {
+    public func shareApp(fromViewController viewController: UIViewController, message: String? = nil, appStoreAppPath: String, completion: ((_ activityType: String?) -> Void)? = nil) {
         let message = message ?? "Check out \(deviceInfoService.appName), an app I've really been enjoying."
         var activityItems: [AnyObject] = [message]
-        if let appLink = NSURL(string: appStoreAppPath) {
-            activityItems.append(appLink)
+        if let appLink = URL(string: appStoreAppPath) {
+            activityItems.append(appLink as AnyObject)
         }
         let shareSheet = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
         shareSheet.completionWithItemsHandler = { activityType, completed, returnedItems, activityError in
             if completed {
-                completion?(activityType: activityType)
+                completion?(activityType.map { $0.rawValue })
             }
         }
-        viewController.presentViewController(shareSheet, animated: true, completion: nil)
+        viewController.present(shareSheet, animated: true, completion: nil)
     }
     
     public func rateApp(fromViewController viewController: UIViewController, iTunesItemIdentifier: Int) {
-        guard let shareURL = NSURL(string: "\(ratingLinkPathPrefix)\(iTunesItemIdentifier)") else { return }
-        UIApplication.sharedApplication().openURL(shareURL)
+        guard let shareURL = URL(string: "\(ratingLinkPathPrefix)\(iTunesItemIdentifier)") else { return }
+        UIApplication.shared.openURL(shareURL)
     }
     
     public func canRateApp() -> Bool {
-        guard let shareURL = NSURL(string: ratingLinkPathPrefix) else { return false }
-        return UIApplication.sharedApplication().canOpenURL(shareURL)
+        guard let shareURL = URL(string: ratingLinkPathPrefix) else { return false }
+        return UIApplication.shared.canOpenURL(shareURL)
     }
     
     public func viewRelatedApp(fromViewController viewController: UIViewController, iTunesItemIdentifier: Int, storeProductViewDelegate: SKStoreProductViewControllerDelegate, completion: (() -> Void)? = nil) {
@@ -80,8 +80,8 @@ private extension SettingsActionService {
     func showStoreProductView(fromViewController viewController: UIViewController, iTunesItemIdentifier: Int, storeProductViewDelegate: SKStoreProductViewControllerDelegate, completion: (() -> Void)? = nil) {
         let store = SKStoreProductViewController()
         store.delegate = storeProductViewDelegate
-        store.loadProductWithParameters([SKStoreProductParameterITunesItemIdentifier: iTunesItemIdentifier]) { success, error in
-            viewController.presentViewController(store, animated: true) {
+        store.loadProduct(withParameters: [SKStoreProductParameterITunesItemIdentifier: iTunesItemIdentifier]) { success, error in
+            viewController.present(store, animated: true) {
                 completion?()
             }
         }
